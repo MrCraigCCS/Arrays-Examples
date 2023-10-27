64252ddff63cd486bbff132ec95d701ee180a7d8
AaronCraig <AaronCraig@TECH08>
2023-10-27 08:43:58 -0700

    Add project files.

---
+++ Arrays_2/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //Array
+
+for (int i = 0; i < numbers.Length; ++i)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+//Get the last index of the array
+int lastIndex = numbers[numbers.Length - 1];
+Console.WriteLine(numbers[lastIndex]);
+
+//Insert at the end of the array
+numbers[numbers.Length] = 928348;
+
+for (int i = 0; i < numbers.Length; ++i)
+{
+    Console.WriteLine(numbers[i]);
+}
